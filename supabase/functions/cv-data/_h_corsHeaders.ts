53544729b4ebcf1bb7cbd84b5234d711963f9046
Daniel <daniel_ciecior@yahoo.de>
2025-09-22 10:01:45 +0200

    region header

--- supabase/functions/cv-data/_h_corsHeaders.ts
+++ supabase/functions/cv-data/_h_corsHeaders.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, Content-Type",
+    "authorization, x-client-info, apikey, Content-Type, x-region",
   "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, PATCH, DELETE",
 };
