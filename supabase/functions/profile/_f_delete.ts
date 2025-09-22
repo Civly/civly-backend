ccf757344ee6fc23da4cf47b0bad73fb7f28cc0b
Daniel <daniel_ciecior@yahoo.de>
2025-09-22 17:18:24 +0200

    delete cvs from user before deleting user

--- supabase/functions/profile/_f_delete.ts
+++ supabase/functions/profile/_f_delete.ts
@@ -4,8 +4,10 @@ import { corsHeaders } from "./_h_corsHeaders.ts";
 export async function deleteProfile(serviceRole, supabaseClient, id: string) {
   const userId = await getUserId(supabaseClient);
   if(id === userId){
-    const { error } = await supabaseClient.from("profiles").delete().eq("id", userId);
+    const { error } = await supabaseClient.from("cv").delete().eq("userId", userId);
     if (error) throw error;
+    const { error: profileError } = await supabaseClient.from("profiles").delete().eq("id", userId);
+    if (profileError) throw profileError;
     const { error: adminError } = await serviceRole.auth.admin.deleteUser(userId);
     if (adminError) throw adminError;
   }
